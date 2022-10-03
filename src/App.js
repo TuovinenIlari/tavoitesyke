ad35164d7eb4d0bd04f78510a645d656a79cb0b6
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-10-03 14:52:29 +0300

    tehtävä tehty

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [age, setAge] = useState(0);
+  const [upheartlim, setUpheartlim] = useState(0);
+  const [loheartlim, setLoheartlim] = useState(0);
+
+  function calculateLimit() {
+    setUpheartlim((220 - age) * 0.85);
+    setLoheartlim((220 - age) * 0.65);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <h2>Heart rate limits calculator</h2>
+        <div class="mb-3">
+          <label for="formGroupExampleInput" class="form-label">
+            Age
+          </label>
+          <input
+            type="number"
+            class="form-control"
+            id="formGroupExampleInput"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          ></input>
+        </div>
+        <div class="mb-3">
+          <label for="formGroupExampleInput2" class="form-label">
+            Heart rate limits
+          </label>
+          <output
+            type="text"
+            class="form-control"
+            id="formGroupExampleInput2"
+            placeholder="Heart rate limits"
+          >
+            {loheartlim.toFixed(0)}-{upheartlim.toFixed(0)}
+          </output>
+        </div>
+        <button type="button" onClick={calculateLimit} class="btn btn-primary">
+          Calculate
+        </button>
+      </form>
     </div>
   );
 }
